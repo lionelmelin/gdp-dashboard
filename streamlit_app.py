a3341e00b3d5a3d5275da0f739b906267a9772d3
Lionel Melin <53967940+lionelmelin@users.noreply.github.com>
2025-01-17 11:31:09 +0000

    Emissions inputs

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,20 @@ import streamlit as st
 import pandas as pd
 import math
 from pathlib import Path
+#### Import Emulator
+from EmulatorCore import Emulator
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='Climate Models',
+    page_icon=':earth_africa:', # This is an emoji shortcode. Could be a URL too.
 )
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
 @st.cache_data
+
 def get_gdp_data():
     """Grab GDP data from a CSV file.
 
@@ -24,48 +27,6 @@ def get_gdp_data():
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
     DATA_FILENAME_GDP = Path(__file__).parent/'data/gdp_data.csv'
     raw_gdp_df = pd.read_csv(DATA_FILENAME_GDP)
-
-    ### Read in emissions data
-    DATA_FILENAME_EMISSIONS = Path(__file__).parent/'data/RCPemissions.xlsx'
-    Emission = pd.read_excel(DATA_FILENAME_EMISSIONS, sheet_name = 'Emission')
-    # Emission = pd.read_excel(r"Data\RCPemissions.xlsx",
-    #                   sheet_name = 'Emission')
-    
-    #### Import Emulator
-    from EmulatorCore import Emulator
-
-    ### choose one emission path
-    emission = Emission['SSP5-34-OS']
-
-    ### Input emissions data to the emulator
-    test_Model = Emulator(Carbon_emission=emission)
-
-    ### Default model prediction
-    fig=test_Model.Plot_Temp()
-
-    # Plot!
-    st.plotly_chart(fig, use_container_width=True)
-
-    ### Add a new model
-    test_Model.update_model('DICE2016')
-    test_Model.update_model('MMM_CMIP5')
-    test_Model.update_model('HadGEM2-ES')
-    test_Model.update_model('INM-CM4')
-
-        ### Default model prediction
-    fig=test_Model.Plot_Temp()
-
-    # Plot!
-    st.plotly_chart(fig, use_container_width=True)
-
-    #### CMIP6 
-    tatm0 = test_Model.CMIP6_prediction()
-
-    ### Default model prediction
-    fig=test_Model.Plot_Temp()
-
-    # Plot!
-    st.plotly_chart(fig, use_container_width=True)
 
     MIN_YEAR = 1960
     MAX_YEAR = 2022
@@ -101,6 +62,63 @@ def get_gdp_data():
 
 gdp_df = get_gdp_data()
 
+def get_data():
+    ### Read in emissions data
+    DATA_FILENAME_EMISSIONS = Path(__file__).parent/'data/RCPemissions.xlsx'
+    Emission = pd.read_excel(DATA_FILENAME_EMISSIONS, sheet_name = 'Emission')
+    # Emission = pd.read_excel(r"Data\RCPemissions.xlsx",
+    #                   sheet_name = 'Emission')
+
+    # DATA_FILENAME_CMIP = Path(__file__).parent/'data/CMIPparas.xlsx'
+    # CMIP = pd.read_excel(DATA_FILENAME_CMIP, sheet_name = 'CMIP')
+    # # CMIP = pd.read_excel(r"Data\CMIPparas.xlsx",
+    # #                       sheet_name = 'CMIP')
+    # CMIP.set_index('Model', inplace=True)
+
+    # DATA_FILENAME_CARBON = Path(__file__).parent/'data/CMIPparas.xlsx'
+    # Carbon = pd.read_excel(DATA_FILENAME_CARBON, sheet_name = 'carbon')
+    # # Carbon =  pd.read_excel(r"Data\CMIPparas.xlsx",
+    # #                       sheet_name = 'carbon')
+    # Carbon.set_index('Model', inplace=True)
+
+    return Emission #, CMIP, Carbon
+
+Emission = get_data()
+
+### choose one emission path
+emission = Emission['SSP5-34-OS']
+
+### Input emissions data to the emulator
+test_Model = Emulator(Carbon_emission=emission)
+
+### Default model prediction
+fig=test_Model.Plot_Temp()
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+### Add a new model
+test_Model.update_model('DICE2016')
+test_Model.update_model('MMM_CMIP5')
+test_Model.update_model('HadGEM2-ES')
+test_Model.update_model('INM-CM4')
+
+    ### Default model prediction
+fig=test_Model.Plot_Temp()
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+#### CMIP6 
+tatm0 = test_Model.CMIP6_prediction()
+
+### Default model prediction
+fig=test_Model.Plot_Temp()
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+
 # -----------------------------------------------------------------------------
 # Draw the actual page
 
@@ -112,9 +130,40 @@ Flexible tool, allowing input of Carbon emission trajectory, to be translated in
 '''
 st.header('Emissions', divider='gray')
 
+with st.popover("Redefine Parameters"):
+    year_range = st.slider("Select a range of years", 2020, 2100, (2020, 2100))
+    # st.write("Year-range:", year_range)
+
+    year_peak = st.slider("Select peak year", 2020, 2100, 2050)
+    # st.write("Peak emissions of xx recorded in", year_peak, ".")
+
+    emission_start = st.number_input(
+        "Start Carbon emissions", value=1, placeholder="Type a number..."
+    )
+    emission_peak = st.number_input(
+        "Peak Carbon emissions", value=4, placeholder="Type a number..."
+    )
+    emission_end = st.number_input(
+        "End Carbon emissions", value=2, placeholder="Type a number..."
+    )
+
+st.write("Start emission of ", emission_start, " recorded in ", year_range[0],
+         "; Peak emissions of ",emission_peak, " recorded in ", year_peak,
+         "; End emissions of ",emission_end, " recorded in ", year_range[1])
+
+# Create emission path, should have years (str) and emission values as columns
+emission = Emission[['Year','SSP5-34-OS']]
+
+# Plot the Emission Path
+st.line_chart(
+    emission,
+    x='Year',
+    y='SSP5-34-OS'
+)
+
 st.header('Temperatures', divider='gray')
 
-list_models = [
+list_CMIP6_models = [
 "CMIP6-MMMean",
 "ACCESS-ESM1-5",
 "AWI-CM-1-1-MR",
@@ -137,17 +186,44 @@ list_models = [
 "NorCPM1",
 "SAM0-UNICON"
 ]
+
+# # Compute the Emission paths for all possible models (CMIP6, maybe CMIP5 also) into a dataframe
+# raw_tatm_df = 
+# # Present the Dataframe with columns: Year, Model, Temperature
+# tatm_df = raw_tatm_df.melt(
+#     ['Model Code'],
+#     [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
+#     'Year',
+#     'Temperature',
+# )
+# # Convert years from string to integers
+# tatm_df['Year'] = pd.to_numeric(tatm_df['Year'])
+
+# Ask the user to select the models they want to display
 container = st.container()
 all = st.checkbox("Select all CMIP6 models")
- 
+
 if all:
-    selected_options = container.pills("Select one or more models:",
-         selection_mode="multi", options=list_models,default=list_models)
+    selected_models = container.pills("Select one or more models:",
+         selection_mode="multi", options=list_CMIP6_models, default=list_CMIP6_models)
 else:
-    selected_options =  container.pills("Select one or more models:",
-        selection_mode="multi", options=list_models)
+    selected_models =  container.pills("Select one or more models:",
+        selection_mode="multi", options=list_CMIP6_models)
 
+# # Filter the dataframe according the the users' choice of models
+# filtered_tatm_df = tatm_df[
+#     (tatm_df['Model'].isin(selected_models))
+#     # & (gdp_df['Year'] <= to_year)
+#     # & (from_year <= gdp_df['Year'])
+# ]
 
+# # Plot the Temperatures
+# st.line_chart(
+#     filtered_tatm_df,
+#     x='Year',
+#     y='Temperature',
+#     color='Model',
+# )
 # Add some spacing
 ''
 ''
